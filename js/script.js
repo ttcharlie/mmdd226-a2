db20ab44bc71faca81ef59adc25997572b80de0d
ttcharlie <126117963+ttcharlie@users.noreply.github.com>
2023-11-27 00:30:57 -0500

    .

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 function changeSofaColor() {
     const sofaViewer = document.getElementById('sofaViewer');
-    sofaViewer.src = "../models/white.glb";
+    sofaViewer.src = "../models/white.glb"; // Example path for a red variant
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+});
