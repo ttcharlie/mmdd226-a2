7425932e5cd3451ed9d78364ab65e22f2a3071cb
ttcharlie <126117963+ttcharlie@users.noreply.github.com>
2023-11-27 00:46:53 -0500

    1

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
-function changeSofaColor() {
-    const sofaViewer = document.getElementById('sofaViewer');
-    sofaViewer.src = "../models/white.glb"; // Example path for a red variant
-}
-
 document.addEventListener('DOMContentLoaded', function() {
+    const sofaViewer = document.getElementById('sofaViewer');
+    const sofaControls = document.getElementById('sofa-controls');
+
+    sofaControls.addEventListener('click', (event) => {
+        const colorString = event.target.dataset.color;
+        const [material] = sofaViewer.model.materials;
+        if (material) {
+            material.pbrMetallicRoughness.setBaseColorFactor(colorString);
+        }
+    });
+
+    // Similar event listeners for chair and table models (if present)
+    // const chairViewer = document.getElementById('chairViewer');
+    // const chairControls = document.getElementById('chair-controls');
+    // ...
+
+    // const tableViewer = document.getElementById('tableViewer');
+    // const tableControls = document.getElementById('table-controls');
+    // ...
 });
