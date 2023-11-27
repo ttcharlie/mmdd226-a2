afe472628fa6d5833eb9079c148b0ae262abc050
ttcharlie <126117963+ttcharlie@users.noreply.github.com>
2023-11-27 12:43:21 -0500

    1

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
-const modelViewer = document.getElementById('colorModel');
-const changeColorBtn = document.getElementById('changeColorBtn');
+document.addEventListener('DOMContentLoaded', function() {
+    const modelViewer = document.getElementById('colorModel');
+    const changeColorBtn = document.getElementById('changeColorBtn');
 
-let currentColor = 'brown'; // Initial color
+    let currentColor = 'brown'; // Initial color
 
-changeColorBtn.addEventListener('click', () => {
-    console.log("Button clicked!"); 
-    // Toggle between different model sources based on the current color
-    if (currentColor === 'brown') {
-        modelViewer.src = '/models/white.glb'; // Change to white model
-        currentColor = 'white';
-    } else {
-        modelViewer.src = '/models/sofa.glb'; // Change back to brown model
-        currentColor = 'brown';
-    }
+    changeColorBtn.addEventListener('click', () => {
+        console.log("Button clicked!"); 
+        // Toggle between different model sources based on the current color
+        if (currentColor === 'brown') {
+            modelViewer.src = '/models/white.glb'; // Change to white model
+            currentColor = 'white';
+        } else {
+            modelViewer.src = '/models/sofa.glb'; // Change back to brown model
+            currentColor = 'brown';
+        }
+    });
 });
