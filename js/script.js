f2ca199ea5e935dfb4925b5e21b23253fa9dc18c
ttcharlie <126117963+ttcharlie@users.noreply.github.com>
2023-11-27 01:53:34 -0500

    2

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 const modelViewer = document.getElementById('colorModel');
 const changeColorBtn = document.getElementById('changeColorBtn');
 
+let currentColor = 'brown'; // Initial color
+
 changeColorBtn.addEventListener('click', () => {
-    // Modify the color property of the model
-    const material = modelViewer.model.materials[0]; // Assuming the color change is applicable to the first material
-    material.pbrMetallicRoughness.baseColorFactor = [0.5, 0.2, 0.8, 1.0]; // Change color to purple, for example
+    // Toggle between different model sources based on the current color
+    if (currentColor === 'brown') {
+        modelViewer.src = 'white.glb'; // Change to blue model
+        currentColor = 'white';
+    } else {
+        modelViewer.src = 'sofa.glb'; // Change back to brown model
+        currentColor = 'brown';
+    }
 });
